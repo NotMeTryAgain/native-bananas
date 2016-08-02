8413aca6873e9d32465d0adadbf64d10436fb428
MichaelHebert <evogashira@gmail.com>
2016-08-01 20:23:31 -0400

    adds styling

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,30 @@
  import React, { Component } from 'react';
  import { AppRegistry, Image, Text, View, StyleSheet } from 'react-native';
 
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+ class LotsOfStyles extends Component {
+   render() {
+     return (
+       <View>
+         <Text style={styles.red}>just red</Text>
+         <Text style={styles.bigblue}>just bigblue</Text>
+         <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+         <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+       </View>
+     )
+   }
+ }
+
  class Blink extends Component {
    constructor(props) {
      super(props);
@@ -21,7 +45,7 @@
    render() {
      let display = this.state.showText ? this.props.text : ' ';
      return (
-       <Text>{display}</Text>
+       <Text style={styles.red}>{display}</Text>
      );
    }
  }
@@ -59,6 +83,7 @@ class Bananas extends Component {
    render() {
      return (
        <View style={{alignItems: 'center'}}>
+        <LotsOfStyles />
         <Bananas />
         <BlinkApp text='Handing it down' />
         <Greeting name='Rexxar' />
